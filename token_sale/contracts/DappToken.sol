b315284907612c5646d3637eb98f85146d40d350
plasmatech8 <com0004@gmail.com>
2021-03-15 01:54:52 +1100

    Fix interface parameter name

--- token_sale/contracts/DappToken.sol
+++ token_sale/contracts/DappToken.sol
@@ -22,8 +22,8 @@ interface IERC20 {
     function transfer(address _to, uint256 _value) external returns (bool);
     function approve(address _spender, uint256 _value) external returns (bool);
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
-    event Transfer(address indexed _from, address indexed _to, uint256 value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 
