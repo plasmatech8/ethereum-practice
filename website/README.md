72c12cbf2fbd32fdd6b173c1ea69f7598003dada
plasmatech8 <com0004@gmail.com>
2021-03-24 02:04:53 +1100

    Completed first part of website

--- website/README.md
+++ website/README.md
@@ -10,3 +10,26 @@ We need to expose our contract APIs to our website.
 * Download [web3 js bundles](https://github.com/ChainSafe/web3.js/tree/1.x/dist)
 * `npm install lite-server`
 * `npm install @truffle/contract` (***)
+
+## 12. ERC20 Crowd Sale Website
+
+* The `bs-config.json` file is used with lite-server. It adds the `token-sale/contracts/build/` path to our resource path.
+
+* The bootstrap, truffle-contract (depreciated), and web3 minified JS files are added to the project.
+  * It would be good to figure out how to import them from npm modules.
+  * Web3 appears to have serious issues with svelte, and potentially other frameworks.
+
+* **Big Number manipulation**: See [here](https://docs.ethers.io/v5/api/utils/bignumber/#BigNumber--methods).
+  * (Note: This is needed for web3 wei conversions. BigInt is not supported because Safari does not support it yet...)
+
+* I have updated `app.js` such that it updates at an interval.
+
+* The `await window.ethereum.enable()` is required to request to connect to MetaMask.
+
+* Need to be **careful of units**
+  * Token price is: smallest unit of DAPP <-> WEI
+  * I added conversion functions between **miniDapp** (BigNumber integer) and **DAPP** (String decimal)
+
+
+TODO: adjust token price so it is what I wanted (0.001 ETH = 1 DAPP)
+TODO: Convert from DAPP to miniDAPP (when token price is fixed) and allow decimal places in the form
--- website/js/app.js
+++ website/js/app.js
@@ -1,8 +1,121 @@
-const web3 = new Web3('ws://localhost:7545');
+App = {
 
-web3.eth.getBalance("0x593CB082767e9B2D97e4C324b41796758A212ccB")
-.then(e => {
-  console.log("accounts[0] has", web3.utils.fromWei(e, "ether"));
-});
+    init: async () => {
+        // Initialise web3 client with wallet
+        try{
+            await window.ethereum.enable();
+        } catch(e) { }
+        App.web3Provider = Web3.givenProvider || 'ws://localhost:7545';
+        web3 = new Web3(App.web3Provider);
 
-console.log("app.js loaded");
+        // Initialise truffle contracts
+        const DappTokenArtefact = await (await fetch('DappToken.json')).json();
+        const DappToken = TruffleContract(DappTokenArtefact);
+        await DappToken.setProvider(App.web3Provider);
+        App.dappToken = await DappToken.deployed();
+
+        const DappTokenSaleArtefact = await (await fetch('DappTokenSale.json')).json();
+        const DappTokenSale = TruffleContract(DappTokenSaleArtefact);
+        await DappTokenSale.setProvider(App.web3Provider);
+        App.dappTokenSale = await DappTokenSale.deployed();
+
+        // Update
+        await App.update();
+        setInterval(App.update, 10000);
+    },
+
+    update: async () => {
+        console.log('---------- UPDATING -----------')
+
+        // Get contract information
+        const tokenPrice = await App.dappTokenSale.tokenPrice();
+        const numTokensSold = await App.dappTokenSale.tokensSold();
+        const numTokensAvailable = await App.dappToken.balanceOf(App.dappTokenSale.address);
+        const decimals = await App.dappToken.decimals();
+        App.tokenDecimals = decimals; // (decimals come first so conversion functions work)
+        App.tokenPrice = tokenPrice;
+        App.numTokensSold = numTokensSold;
+        App.numTokensAvailable = numTokensAvailable;
+        App.numTokensTotal = numTokensAvailable.add(numTokensSold);
+
+        console.log({
+            'Token Sale Address:': App.dappTokenSale.address,
+            'Token Sale Price:': web3.utils.fromWei(tokenPrice, 'ether') + ' ETH',
+            'Token Sale Num Tokens Sold:': App.toDapp(numTokensSold),
+            'Token Sale Num Tokens Held:': App.toDapp(numTokensAvailable),
+            'Token Decimals:': decimals.toString()
+        });
+
+        // Initialise web3 wallet accounts
+        const accounts = await web3.eth.getAccounts();
+        const etherBalance = await web3.eth.getBalance(accounts[0]);
+        const dappBalance = await App.dappToken.balanceOf(accounts[0]);
+        App.myAccount = accounts[0];
+        App.myDappBalance = dappBalance;
+        App.myEtherBalance = etherBalance;
+
+        console.log({
+            'Accounts:': accounts,
+            'Account has ETH:': web3.utils.fromWei(etherBalance, 'ether'),
+            'Account has DAPP:': App.toDapp(dappBalance),
+        });
+
+        // Update page render
+        App.render();
+    },
+
+    render: () => {
+        console.log('Rendering...')
+        document.querySelector('#accountAddress').innerText = App.myAccount;
+        document.querySelector('#dappTokenAddress').innerText = App.dappToken.address;
+        document.querySelector('#dappTokenSaleAddress').innerText = App.dappTokenSale.address;
+
+        document.querySelectorAll('.token-price'        ).forEach(e => e.innerText = web3.utils.fromWei(App.tokenPrice, 'ether'));
+        document.querySelectorAll('.dapp-balance'       ).forEach(e => e.innerText = App.toDapp(App.myDappBalance      ));
+        document.querySelectorAll('.tokens-available'   ).forEach(e => e.innerText = App.toDapp(App.numTokensAvailable ));
+        document.querySelectorAll('.tokens-sold'        ).forEach(e => e.innerText = App.toDapp(App.numTokensSold      ));
+        document.querySelectorAll('.tokens-total'       ).forEach(e => e.innerText = App.toDapp(App.numTokensTotal     ));
+
+        const progressBar = document.querySelector('#progressBar')
+        progressBar.setAttribute('aria-valuenow', App.numTokensSold);
+        progressBar.setAttribute('aria-valuemax', App.numTokensTotal);
+        const percentage = Math.floor((App.numTokensSold.toNumber() / App.numTokensTotal.toNumber())*100) + "%";
+        progressBar.style.width = percentage;
+        progressBar.innerText = percentage;
+
+        document.querySelector('#loader').style.display = 'none';
+        document.querySelector('#content').style.display = 'block';
+    },
+
+    onFormUpdate: () => {
+
+        // TODO: Convert from DAPP to miniDAPP (when token price is fixed)
+        // TODO: allow decimal places in the form
+
+        const numTokensToBuy = document.querySelector('#numberOfTokens').value;
+        const purchaseNotice = document.querySelector('#purchaseNotice');
+        if(numTokensToBuy) {
+            const amount = new web3.utils.BN(numTokensToBuy);
+            const price = App.tokenPrice;
+            const charge = web3.utils.fromWei(amount.mul(price), 'ether');
+            purchaseNotice.innerText = `Buy ${amount} miniDAPP for ${charge} ETH`
+        } else {
+            purchaseNotice.innerText = `Invalid token amount`
+        }
+    },
+
+    toDapp: amt => {
+        const factor = new web3.utils.BN(10).pow(new web3.utils.BN(App.tokenDecimals));
+        const d = amt.div(factor);
+        const m = amt.mod(factor);
+        return `${d}.${m}` // Returns String (decimal)
+    },
+
+    toMiniDapp: amt => {
+        const factor = new web3.utils.BN(10).pow(new web3.utils.BN(App.tokenDecimals));
+        return new web3.utils.BN(amt).mul(factor); // Returns BigNumber (integer)
+    }
+
+}
+
+window.onload = App.init;
